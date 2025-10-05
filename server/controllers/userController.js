679cf84aa671be86f4cc55127c01a0d2b04aeae5
Bhashitha  Samarasinghe <bhashithasamarasinghe@gmail.com>
2025-10-05 23:54:52 +0530

    Create userController.js

---
+++ server/controllers/userController.js
@@ -0,0 +1,23 @@
+import User from " .. /models/User.js";
+
+
+
+// Signup a new user
+export const signup = async (req, res)=>{
+    const { fullName, email, password, bio } = req.body;
+
+    try {
+        if (!fullName || !email || !password || !bio) {
+            return res.json({ success: false, message: "Missing Details" });
+        }
+
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.json({ success: false, message: "Account already exists" });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({ fullName, email, password: hashPassword, bio });
+    } catch (error) {}}
